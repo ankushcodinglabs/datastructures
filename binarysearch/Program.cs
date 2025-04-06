b1146040d0a248cf49214a5f763bad2b35a2c8c6
Ankush Gupta <ankush.lbsim@gmail.com>
2025-04-06 18:49:09 -0400

    Added the code for binary search

---
+++ binarysearch/Program.cs
@@ -0,0 +1,22 @@
+﻿static bool BinarySearch(int[] array, int number)
+{
+    int min = 0;
+    int max = array.Length - 1;
+    while (min <= max) {
+        int middle = (min + max) / 2;
+        if (array[middle] == number) {
+            return true;
+        } else if (array[middle] < number) {
+            min = middle + 1;
+        } else {
+            max = middle - 1;
+        }
+    }
+
+    return false;
+}
+
+Console.WriteLine("An implementation for binary search in C#");
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+Console.WriteLine($"Searching number 3: {BinarySearch(array, 3)}");
